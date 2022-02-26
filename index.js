7fa5b820cb85cb3d443c1a59e6a0d2e49e107b25
Woogster <john.m.wooley@gmail.com>
2022-02-26 16:38:27 -0600

    Fixed local scope in generateCard function

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const inquirer = require('inquirer');
+const generatePage = require('./src/createEmployeePage');
+const fs = require('fs');
+
+const employees = [];
+
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('dist/index.html', data, err => {
+            if(err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true, 
+                message: 'Your HTML file has been created :)'
+            });
+        });
+    });
+}
 
 const init = function() {
     return inquirer.prompt([
@@ -22,26 +41,116 @@ const init = function() {
             name: 'employeeEmail',
             message: 'Please enter the employee email: ',
             validate: function(employeeEmail) {
-                return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                if(employeeEmail) {
+                    return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(employeeEmail);
+                }
+                else {
+                    console.log('Invalid format. Please enter in a valid email:');
+                    return false;
+                }
             }
-        }
-    ])
-}
-
-const managerPrompt = function() {
-    return inquirer.prompt([
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: 'Please select the type/position of the employee:',
+            choices: [
+                "employee",
+                "manager",
+                "engineer",
+                "intern"
+            ]
+        },
         {
             type: 'input',
             name: 'phoneNumber',
             message: `What is the manager's office number?`,
             validate: phoneNumber => {
-                
-            }
+                if(phoneNumber.length >= 3 || phoneNumber.length <= 10) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a valid phone number :");
+                    return false;
+                }    
+            },
+            when: (answers) => answers.employeeType === "manager"
         },
+        {
+            type: 'input',
+            name: 'school',
+            message: `Please input the name of the intern's school:`,
+            validate: school => {
+                if(school){
+                    return true;
+                }
+                else {
+                    console.log('Please enter a valid school name: ');
+                    return false;
+                }
+            },
+            when: (answers) => answers.employeeType === "intern"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your github username:',
+            validate: github => {
+                if (github) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter in a valid username:');
+                    return false;
+                }
+            },
+            when: (answers) => answers.employeeType === "engineer"
+
+        }
     ])
 }
 
-init()
-    .then(employeeData => {
-        console.log(data)
-    })
+const runProgram = function() {
+    
+    init()
+        .then(data => {
+            employees.push(data);
+
+            return inquirer.prompt(
+                {
+                    type: 'confirm',
+                    name: 'confirmNewEmployee',
+                    message: 'Would you like to add another employee?'
+                }
+            )
+        })
+        .then(employeeConfirm => {    
+            if(employeeConfirm.confirmNewEmployee){
+                runProgram();
+            }
+            // console.log(employees);
+            return generatePage(employees);
+        })
+        .then(generatedHTMLData => {
+            console.log(generatedHTMLData);
+            writeToFile('/dist/index.html', generatedHTMLData);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+runProgram();
+// const employees_new = [
+//     {
+//       employeeName: 'John',
+//       employeeEmail: 'jay@dem.com',
+//       employeeType: 'employee'
+//     },
+//     {
+//       employeeName: 'Johnny',
+//       employeeEmail: 'jaywooski@google.co',
+//       employeeType: 'engineer',
+//       github: 'jaywooski'
+//     }
+//   ];
---
+++ src/createEmployeePage.js
@@ -0,0 +1,68 @@
+const generateCard = function(array) {
+    var cardSection = "";
+    
+    for(let i = 0; i < array.length; i++) {
+        
+        cardSection += `
+                
+        <!-- card#${i+1} -->
+        <div class="shadow-md my-12 text-white rounded-lg">
+            <div class="bg-blue-700 rounded-t-lg">
+                <ul class="flex-col justify-around">
+                    <li class="p-2 text-xl">${array[i].employeeName}</li>
+                    <li class="p-2 text-lg">${array[i].employeeType}</li>
+                </ul>
+            </div>
+            <div>
+                <ul class="flex-col text-sm text-black p-4 ">
+                    <li class="border p-2">ID:${i + 1}</li>
+                    <li class="border p-2">Email:<a href='mailto:${array[i].employeeEmail}' class='px-2'>${array[i].employeeEmail}</a></li>
+                    ${generateThirdDetail(array[i])}
+                </ul>
+            </div>
+        </div>`
+
+    }
+
+    return cardSection;
+} 
+
+const generateThirdDetail = function(obj) {
+    const type = obj.employeeType
+    switch(type) {
+        case 'manager':
+            return `<li class="border p-2">Office Number: ${obj.phoneNumber}</li>`;
+        case 'engineer':
+            return `<li class="border p-2">Github: <a href="www.github.com/${obj.github}">${obj.github}</a></li>`;
+        case 'intern':
+            return `<li class="border p-2">School: ${obj.school}</li>`
+        default:
+            return `<li class="border p-2">An ${type}</li>`
+    }
+}
+
+const generatePage = function(array){
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Employees</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"
+     integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header class="p-16 text-3xl text-white flex justify-center bg-red-700">
+        <h1>My Team</h1>
+    </header>
+    <div class="flex flex-col mx-24 sm:flex-row sm:mx-0 justify-around " >
+        ${generateCard(array)}
+    </div>
+
+</body>
+</html>`
+}
+
+module.exports = generatePage;
